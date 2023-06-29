0ad88758d43aa7d64b3b993789cb87837dc46c34
adelbrx <adelbrixi1@gmail.com>
2023-06-29 19:33:58 +0200

    first version of this project

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '74680644-162c-4c47-827b-b68b10ebb511';
+export const MODAL_CLOSE_SEC = 2.5;
